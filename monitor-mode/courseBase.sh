30242a100fb30acfc66e93b7e80df94a28e192d1
singe <singe-github@singe.za.net>
2018-11-27 22:43:20 +0200

    Files not getting coppied, fiddling ..

--- monitor-mode/courseBase.sh
+++ monitor-mode/courseBase.sh
@@ -4,4 +4,4 @@ modprobe mac80211_hwsim
 crontab -u root /tmp/crontab.tmp
 rm -rf /tmp/crontab.tmp
 /etc/init.d/cron start
-gunzip /usr/local/share/sensepost.cap.gz
+curl -o /usr/local/share/sensepost.cap "https://wiki.wireshark.org/SampleCaptures?action=AttachFile&do=get&target=wpa-Induction.pcap"
