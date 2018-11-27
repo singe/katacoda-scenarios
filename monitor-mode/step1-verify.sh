4ff7c95b04345cdd61c6531babe676b5c89f31b3
singe <singe-github@singe.za.net>
2018-11-27 23:25:10 +0200

    step1 verification script debugging

--- monitor-mode/step1-verify.sh
+++ monitor-mode/step1-verify.sh
@@ -1,1 +1,1 @@
-(( $(dpkg -l aircrack-ng|grep -e ii -e un|grep ^ii 2>/dev/null | wc -l) >= 1 )) && echo \"done\"
+(( $(dpkg -l aircrack-ng|grep -e ii -e un|grep ^ii 2>/dev/null | wc -l) >= 1 )) && echo "done"
