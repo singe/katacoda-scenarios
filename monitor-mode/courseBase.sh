86f27505907d8ffc75afd0c4532e4f46ce9956a9
singe <singe-github@singe.za.net>
2018-11-28 14:42:09 +0200

    trying to understand client and host01 setup

--- monitor-mode/courseBase.sh
+++ monitor-mode/courseBase.sh
@@ -1,9 +1,8 @@
-ssh root@[[HOST_IP]] "echo bar > /tmp/bar"
-ssh root@[[HOST_IP]] "echo baz > /tmp/baz && touch /tmp/foo"
-ssh root@[[HOST_IP]] "echo quux > /tmp/quux; touch /tmp/zero"
+ssh root@[[HOST_IP]] "echo baz > /tmp/baz && \
+touch /tmp/foo"
+ssh root@[[HOST_IP]] "apt-get install -y tcpreplay"
 ssh root@[[HOST_IP]] "modprobe mac80211_hwsim radios=1"
 ssh root@[[HOST_IP]] "crontab -u root /tmp/crontab.tmp"
 ssh root@[[HOST_IP]] "rm -rf /tmp/crontab.tmp"
 ssh root@[[HOST_IP]] "/etc/init.d/cron start"
 ssh root@[[HOST_IP]] "curl -o /usr/local/share/sensepost.cap 'https://wiki.wireshark.org/SampleCaptures?action=AttachFile&do=get&target=wpa-Induction.pcap'"
-ssh root@[[HOST_IP]] "apt-get install -y tcpreplay"
