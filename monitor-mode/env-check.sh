9ad281771d4de4b120f439f03fc5a7567855be66
singe <singe-github@singe.za.net>
2018-11-28 15:49:51 +0200

    Added a foreground check that the environment is set up

--- monitor-mode/env-check.sh
+++ monitor-mode/env-check.sh
@@ -1,4 +1,4 @@
-#"Please wait while we finish setting up your environment."
+echo -e "\ec\u001b[32mPlease wait while we finish setting up your environment.\u001b[m"
 while ((1)); do clear; which tcpreplay && break; sleep 1; done > /dev/null 2> /dev/null
 while ((1)); do clear; iw dev wlan0 info && break; sleep 1; done > /dev/null 2> /dev/null
 echo "All set up."
