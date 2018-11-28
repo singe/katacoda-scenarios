6978ef75d0ac30e2649632046ce0a758d056c33b
singe <singe-github@singe.za.net>
2018-11-28 15:28:57 +0200

    fixed quotes in step2-verify

--- monitor-mode/step2-verify.sh
+++ monitor-mode/step2-verify.sh
@@ -1,1 +1,1 @@
-ssh root@[[HOST_IP]] "[ $(iw dev mon0 info|grep type|cut -dm -f2) == "onitor" ] || [ $(iw dev wlan0 info|grep type|cut -dm -f2) == "onitor"] && echo done"
+ssh root@[[HOST_IP]] '[ $(iw dev mon0 info|grep type|cut -dm -f2) == "onitor" ] || [ $(iw dev wlan0 info|grep type|cut -dm -f2) == "onitor"] && echo done'
