da7f2a0bf01554711b63fc87bc954ece0eecbe30
singe <singe-github@singe.za.net>
2018-11-28 12:04:09 +0200

    Change environment setup to use ssh

--- monitor-mode/courseBase.sh
+++ monitor-mode/courseBase.sh
@@ -1,8 +1,6 @@
-#!/bin/sh
-sleep 1
-modprobe mac80211_hwsim radios=1
-crontab -u root /tmp/crontab.tmp
-rm -rf /tmp/crontab.tmp
-/etc/init.d/cron start
-curl -o /usr/local/share/sensepost.cap "https://wiki.wireshark.org/SampleCaptures?action=AttachFile&do=get&target=wpa-Induction.pcap"
-apt-get install -y tcpreplay
+ssh root@[[HOST_IP]] "modprobe mac80211_hwsim radios=1 \
+  && crontab -u root /tmp/crontab.tmp \
+  && rm -rf /tmp/crontab.tmp \
+  && /etc/init.d/cron start \
+  && curl -o /usr/local/share/sensepost.cap 'https://wiki.wireshark.org/SampleCaptures?action=AttachFile&do=get&target=wpa-Induction.pcap' \
+  && apt-get install -y tcpreplay"
