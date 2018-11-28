73d515705cbb7522df22a9c81888135cfc2d4e6c
singe <singe-github@singe.za.net>
2018-11-28 15:53:28 +0200

    Added a foreground check that the environment is set up

--- monitor-mode/env-check.sh
+++ monitor-mode/env-check.sh
@@ -1,5 +1,5 @@
 echo -e "\ec \u001b[32mPlease wait while we finish setting up your environment.\u001b[m"
-while ((1)); do clear; which tcpreplay && break; sleep 1; done
-while ((1)); do clear; iw dev wlan0 info && break; sleep 1; done
+while ((1)); do clear; which tcpreplay && break; sleep 1; done 2> /dev/null
+while ((1)); do clear; iw dev wlan0 info && break; sleep 1; done 2> /dev/null
 echo "All set up."
 echo -e "\ec \u001b[32mYour Interactive Bash Terminal. A safe place to learn and execute commands.\u001b[m"
